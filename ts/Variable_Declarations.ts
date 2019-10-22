8775d2eb0c296789c0c90b00b2a4c4fbc895aabf
Yafei Guo <yafei.guo@ygomi.com>
2019-10-22 17:49:00 +0800

    VD and interface

--- ts/Variable_Declarations.ts
+++ ts/Variable_Declarations.ts
@@ -14,3 +14,72 @@ function f(input: boolean) {
     // Error: 'b' doesn't exist here
     return b;
 }
+
+// catch语句里声明的变量也具有同样的作用域规则
+
+try {
+    throw "exception"
+}
+catch (e){
+    console.log('error')
+}
+// error: 'e' doesn't exist here
+// console.log(e)
+
+// 暂时性死区 变量在定义之前不能使用它
+let a: number = 1
+a++
+
+function foo() {
+    // okay to capture 'exist'
+    return exist;
+}
+
+// 不能在'exist'被声明前调用'foo'
+// 运行时应该抛出错误
+let exist: string = "123";
+foo();
+
+// 并不是说块级作用域变量不能用函数作用域变量来声明。 而是块级作用域变量需要在明显不同的块里声明
+function foo1(foo: number){
+    let foo1: number = 100
+}
+
+function foo2(condition: boolean,foo:number){
+    if(condition){
+        let foo2: number = 100;
+        return foo2;
+    }
+    return foo2;
+}
+
+// 块级作用域变量的获取
+function theCityThatAlwaysSleeps() {
+    let getCity;
+
+    if (true) {
+        let city: string = "Seattle";
+        getCity = function() {
+            return city;
+        }
+    }
+
+    return getCity();
+}
+
+// const 声明变量值不可以被重新赋值
+const numLivesForCat = 9;
+const kitty = {
+    name: "Aurora",
+    numLives: numLivesForCat,
+}
+// Error
+// kitty = {
+//     name: "Danielle",
+//     numLives: numLivesForCat
+// };
+// all "okay"
+kitty.name = "Rory";
+kitty.name = "Kitty";
+kitty.name = "Cat";
+kitty.numLives--;
---
+++ ts/interface.js
@@ -0,0 +1,103 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+function printLabel(labelledObj) {
+    console.log(labelledObj.label);
+}
+var myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
+function createSquare(config) {
+    var newSquare = { color: "white", width: 10, area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var mySquare = createSquare({ color: "black" });
+var p1 = { x: 10, y: 20 };
+var mySearch;
+mySearch = function (source, subString) {
+    var result = source.search(subString);
+    return result > -1;
+};
+// 参数名不需要与接口定义中相同
+var mySearch1;
+mySearch1 = function (src, sub) {
+    var result = src.search(sub);
+    return result > -1;
+};
+// 类型系统自动推断参数类型，类型检查器自动检查返回值类型
+var mySearch2;
+mySearch2 = function (src, sub) {
+    var result = src.search(sub);
+    return result > -1;
+};
+var myArray;
+myArray = ["Bob", "Fred"];
+var myStr = myArray[0];
+var myStr1 = myArray[1];
+var Clock = /** @class */ (function () {
+    function Clock(h, m) {
+    }
+    return Clock;
+}());
+var Clock1 = /** @class */ (function () {
+    function Clock1(h, m) {
+    }
+    Clock1.prototype.setTime = function (d) {
+        this.currentTime = d;
+    };
+    return Clock1;
+}());
+var square = {};
+square.color = "blue";
+square.sideLength = 10;
+var square1 = {};
+square1.color = "blue";
+square1.sideLength = 10;
+square1.penWidth = 5.0;
+function getCounter() {
+    var counter = function (start) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    return counter;
+}
+var c10 = getCounter();
+c10(10);
+c10.reset();
+c10.interval = 5.0;
+// 接口继承类
+var Control = /** @class */ (function () {
+    function Control() {
+    }
+    return Control;
+}());
+var Button = /** @class */ (function (_super) {
+    __extends(Button, _super);
+    function Button() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Button.prototype.select = function () { };
+    return Button;
+}(Control));
+var TextBox = /** @class */ (function (_super) {
+    __extends(TextBox, _super);
+    function TextBox() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    TextBox.prototype.select = function () { };
+    return TextBox;
+}(Control));
